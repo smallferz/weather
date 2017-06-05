d5bea5ddeddb26956d34d6c62777fb1467056ecb
smallferz <smallferz@gmail.com>
2017-06-05 17:24:20 +0300

    fix work simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,9 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+require 'simplecov'
+SimpleCov.start
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
